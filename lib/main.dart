fa13c02e874edcaf1df4477cae4e51e304275530
linnefromice <linnefromice@gmail.com>
2020-01-21 00:33:22 +0900

    first commit (prototype / exists error)

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice_bloc/screen/counter_screen.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CounterScreen();
+  }
+}
